b4e6269a296922111bc584de314fb2ef1bcf0e35
yadilis <yadirapalomo05@gmail.com>
2025-05-29 00:03:50 -0500

    Taller 1 cambios finales

---
+++ src/main/java/com/Taller_Ps/Servicio/EmailServicio.java
@@ -0,0 +1,21 @@
+package com.Taller_Ps;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Servicio {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void enviarCorreo(String destinatario, String asunto, String mensaje) {
+        SimpleMailMessage correo = new SimpleMailMessage();
+        correo.setTo(destinatario);
+        correo.setSubject(asunto);
+        correo.setText(mensaje);
+        mailSender.send(correo);
+    }
+}
